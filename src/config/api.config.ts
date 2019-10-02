cde254996b268fa0685e534328eecf9412549bf1
guilhermeuberti <guilherme.uberti@hotmail.com>
2019-10-02 15:14:02 -0300

    Mostrando lista de categorias com as imagens

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,3 +1,7 @@
 export const API_CONFIG = {
-    baseUrl: "http://localhost:8080"
+
+    baseUrl: "http://localhost:8080",
+    bucketBaseUrl: "http://s3-sa-east-1.amazonaws.com/user-group-imagens"
+    
 }
+
