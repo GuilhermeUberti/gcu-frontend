75cf9b9e96c5776a975e115ce1436701acc9348b
guilhermeuberti <guilherme.uberti@hotmail.com>
2019-10-10 13:27:30 -0300

    Povoando Estado e Cidade dinâmicamente

--- src/pages/signut/signut.module.ts
+++ src/pages/signut/signut.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { SignutPage } from './signut';
+import { CidadeService } from '../../services/domain/cidade.service';
+import { EstadoService } from '../../services/domain/estado.service ';
 
 @NgModule({
   declarations: [
@@ -9,5 +11,9 @@ import { SignutPage } from './signut';
   imports: [
     IonicPageModule.forChild(SignutPage),
   ],
+  providers: [
+    CidadeService,
+    EstadoService
+  ]
 })
 export class SignutPageModule {}
