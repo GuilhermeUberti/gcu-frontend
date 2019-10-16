38fda74b200085fda30196d228cced6fffc30861
guilhermeuberti <guilherme.uberti@hotmail.com>
2019-10-16 14:09:01 -0300

    adicionando novo Cliente

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -3,6 +3,8 @@ import { Observable } from "rxjs/Rx";
 import { Injectable } from "@angular/core";
 import { StorageService } from "../services/storage.service";
 import { AlertController } from "ionic-angular";
+import { FieldMessage } from "../models/fieldmessage";
+import { MessageSpan } from "@angular/compiler/src/i18n/i18n_ast";
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
@@ -28,15 +30,17 @@ export class ErrorInterceptor implements HttpInterceptor {
             switch(errorObj.status) {
                 case 401:
                     this.handle401();
-                    break;
-            }
+                    break;      
 
-            switch(errorObj.status) {
                 case 403:
                     this.handle403();
                     break;
 
-            default:
+                case 422:
+                    this.handle422(errorObj);
+                    break;
+
+                default:
                     this.handleDefaultError(errorObj);
             }
 
@@ -62,6 +66,20 @@ export class ErrorInterceptor implements HttpInterceptor {
         alert.present();
     }
 
+    handle422(errorObj) {
+        let alert = this.alertCtrl.create({
+            title : 'Erro 422: Validação',
+            message: this.listErros(errorObj.errors),
+            enableBackdropDismiss : false,
+            buttons : [
+                {
+                    text: 'Ok'
+                }
+            ]
+        });
+        alert.present();
+    }
+
     handleDefaultError(errorObj) {
         let alert = this.alertCtrl.create({
             title : 'Erro ' + errorObj.status + ': ' + errorObj.error,
@@ -74,6 +92,14 @@ export class ErrorInterceptor implements HttpInterceptor {
             ]
         });
         alert.present();
+    }
+
+    private listErros(messages : FieldMessage[]) : string {
+        let s : string = '';
+        for(var i = 0; i < messages.length; i++) {
+            s = s + '<p><strong>' + messages[i].fieldName + "</strong>: " + messages[i].message + '<\p>';
+        }
+        return s;
     }
 }
 
