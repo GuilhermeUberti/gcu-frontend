c4ed454c93a0564e6d7bc48f67f6c8a67240e8e0
guilhermeuberti <guilherme.uberti@hotmail.com>
2019-10-23 15:31:03 -0300

    informando pedido ao usuario

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -6,6 +6,7 @@ import { CartService } from '../../services/domain/cart.service';
 import { ClienteDTO } from '../../models/cliente.dto';
 import { EnderecoDTO } from '../../models/endereco.dto';
 import { ClienteService } from '../../services/domain/cliente.service';
+import { PedidoService } from '../../services/domain/pedido.service';
 
 @IonicPage()
 @Component({
@@ -18,12 +19,14 @@ export class OrderConfirmationPage {
   cartItems : CartItem[];
   cliente : ClienteDTO;
   endereco : EnderecoDTO;
+  codpedido : string;
 
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
     public cartService : CartService,
-    public clienteService : ClienteService) {
+    public clienteService : ClienteService,
+    public pedidoService : PedidoService) {
 
     this.pedido = this.navParams.get('pedido');
   }
@@ -48,5 +51,31 @@ export class OrderConfirmationPage {
 
   total(){
     return this.cartService.total();
+  }
+
+  back() {
+    this.navCtrl.setRoot('CartPage');
+  }
+
+  home() {
+    this.navCtrl.setRoot('CategoriasPage');
+  }
+
+  checkout(){
+    this.pedidoService.insert(this.pedido)
+      .subscribe(response => {
+        this.cartService.createOrClearCart();
+        this.codpedido = this.extractId(response.headers.get('location'));
+      },
+      error => {
+        if (error.status == 403) {
+          this.navCtrl.setRoot('HomePage');
+        }
+      });
+  }
+
+  private extractId(location : string) {
+    let position = location.lastIndexOf('/');
+    return location.substring(position + 1, location.length);
   }
 }
